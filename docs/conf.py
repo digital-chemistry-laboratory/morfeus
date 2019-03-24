6493be8ba29acd19688e69e393413b1c506f95ab
Kjell Jorner <kjell.jorner@gmail.com>
2019-03-24 19:37:40 +0000

    Added mock imports to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -43,6 +43,8 @@ extensions = [
     'sphinx.ext.napoleon',
 ]
 
+autodoc_mock_imports = ["numpy", "scipy", "matplotlib", "rdkit"]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
