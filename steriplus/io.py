fdc4ed953231be8ce4b75bb35fba9089c28f3347
Kjell Jorner <kjell.jorner@gmail.com>
2019-05-12 20:39:29 +0100

    Refactored io

--- steriplus/io.py
+++ steriplus/io.py
@@ -1,50 +1,29 @@
-"""This module contains geometry file parsing functions.
+"""Geometry file parsing functions.
 
 Functions:
     read_gjf: Parses Gaussian input files.
     read_xyz: Parses xyz files.
 """
-
 from steriplus.data import atomic_symbols
 
-def read_xyz(filename):
-    """Reads xyz file and returns element_ids as they are written in the xyz
-    file (either atomic numbers or symbols).
-
-    Args:
-        filename (str)          :     Name of xyz file
-    Returns:
-        element_id_list (list)  :     List of element ids (atomic numbers or
-                                      symbols)
-        coord_list (list)       :     List of atomic coordinates in Å
-    """
-    lines = open(filename).readlines()[2:]
-    lines = [line.strip().split() for line in lines if line.strip().split()]
-    element_id_list = []
-    coord_list = []
-    for line in lines:
-        atom = line[0]
-        if atom.isdigit():
-            atom = int(atom)
-        element_id_list.append(atom)
-        coord_list.append([float(line[1]), float(line[2]), float(line[3])])
-    return element_id_list, coord_list
-
 def read_gjf(filename):
-    """Reads gjf/com file and returns element_ids as they are written in the
-    file (either atomic numbers or symbols).
+    """Reads Gaussian gjf/com file and returns elements as they are written in the
+    file (either atomic numbers or symbols) as well as coordinates.
 
     Args:
-        filename (str)          :     Name of xyz file
+        filename (str): Name of gjf/com file
+
     Returns:
-        element_id_list (list)  :     List of element ids (atomic numbers or
-                                      symbols)
-        coord_list (list)       :     List of atomic coordinates in Å
+        elements (list): Elements as atomic symbols or numbers
+        coordinates (list): Coordinates (Å)
     """
+    # Read file and split lines
     lines = open(filename).readlines()
     lines = [line.strip().split() for line in lines]
-    element_id_list = []
-    coord_list = []
+
+    # Loop over lines and store elements and coordinates 
+    elements = []
+    coordinates = []
     empty_counter = 0
     read_counter = 0
     for line in lines:
@@ -55,7 +34,35 @@ def read_gjf(filename):
                 atom = line[0]
                 if atom.isdigit():
                     atom = int(atom)
-                element_id_list.append(atom)
-                coord_list.append([float(line[1]), float(line[2]), float(line[3])])
+                elements.append(atom)
+                coordinates.append([float(line[1]), float(line[2]),
+                                    float(line[3])])
             read_counter += 1
-    return element_id_list, coord_list
+    return elements, coordinates
+
+def read_xyz(filename):
+    """Reads xyz file and returns elements as they are written in the xyz
+    file (either atomic numbers or symbols) as well as coordinates.
+
+    Args:
+        filename (str): Name of xyz file
+
+    Returns:
+        elements (list): Elements as atomic symbols or numbers
+        coordinates (list): Coordinates (Å)
+    """
+    # Read file and split lines
+    lines = open(filename).readlines()[2:]
+    lines = [line.strip().split() for line in lines if line.strip().split()]
+
+    # Loop over lines and store elements and coordinates
+    elements = []
+    coordinates = []
+    for line in lines:
+        atom = line[0]
+        if atom.isdigit():
+            atom = int(atom)
+        elements.append(atom)
+        coordinatess.append([float(line[1]), float(line[2]), float(line[3])])
+    
+    return elements, coordinates
