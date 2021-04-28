ec1436027900fedecafaf9e594ddf647ba42ce82
Kjell Jorner <kjell.jorner@gmail.com>
2021-04-28 21:12:23 +0100

    Deprecate .percent_buried_volume
    - Deprecate .percent_buried_volume
    - Add .fraction_buried_volume

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+
+
+
+### Deprecated
+- ``BuriedVolume.percent_buried_volume``. Use ``BuriedVolume.fraction_buried_volume`` instead. ``percent_buried_volume`` will be reintroduced later with the proper meaning.
+
--- tests/test_buried_volume.py
+++ tests/test_buried_volume.py
@@ -15,7 +15,7 @@ def test_one():
     """Test one complex."""
     elements, coordinates = read_xyz(DATA_DIR / "xyz" / "1.xyz")
     bv = BuriedVolume(elements, coordinates, 1, excluded_atoms=[1, 2, 3, 4, 5, 6, 7])
-    assert_almost_equal(bv.percent_buried_volume * 100, 29.6, decimal=1)
+    assert_almost_equal(bv.fraction_buried_volume * 100, 29.6, decimal=1)
 
 
 def pytest_generate_tests(metafunc):
@@ -38,5 +38,5 @@ def test_reference(bv_data):
     bv = BuriedVolume(elements, coordinates, 1, excluded_atoms=excluded_atoms)
 
     assert_almost_equal(
-        bv.percent_buried_volume * 100, percent_buried_volume_ref, decimal=0
+        bv.fraction_buried_volume * 100, percent_buried_volume_ref, decimal=0
     )
