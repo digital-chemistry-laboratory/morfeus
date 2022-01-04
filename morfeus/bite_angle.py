890cf33cdd62af3a357630bb9d5eba84ad8e9380
Kjell Jorner <kjell.jorner@gmail.com>
2022-01-04 15:52:03 +0100

    Added ref_atoms to BiteAngle

--- morfeus/bite_angle.py
+++ morfeus/bite_angle.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import Optional
+from typing import Optional, Sequence
 
 import numpy as np
 
@@ -14,16 +14,19 @@ class BiteAngle:
 
     The bite angle can become 'inverted' if it goes beyond 180°. This cannot be detected
     automatically, so the user has to supply a reference vector that should point in the
-    direction of the ligand 'center of mass'.
+    'direction' of the ligand. As a convenience, the vector can be constructed
+    automatically from the metal atom to the geometric mean of the given 'ref_atoms'.
 
     Args:
         coordinates: Coordinates (Å)
-        metal_index: Index of metal atom
-        ligand_index_1: Index of first ligand atom
-        ligand_index_2: Index of second ligand atom
+        metal_index: Index of metal atom (1-indexed)
+        ligand_index_1: Index of first ligand atom (1-indexed)
+        ligand_index_2: Index of second ligand atom (1-indexed)
+        ref_vector: Reference vector for determining inverted bite angle
+        ref_atoms: Reference atoms for determnining inverted bite angle (1-indexed)
 
     Attributes:
-        angle: Bite angle
+        angle: Bite angle (degrees)
         inverted: Whether angle is 'inverted'
     """
 
@@ -36,23 +39,38 @@ class BiteAngle:
         metal_index: int,
         ligand_index_1: int,
         ligand_index_2: int,
+        ref_atoms: Optional[Sequence[int]] = None,
         ref_vector: Optional[ArrayLike1D] = None,
     ) -> None:
+        # Check keywords
+        if ref_atoms is not None and ref_vector is not None:
+            raise ValueError("ref_atoms and ref_vector cannot be set at the same time.")
+
         # Construct vectors
         v_1 = coordinates[ligand_index_1 - 1] - coordinates[metal_index - 1]
-        v_1 /= np.linalg.norm(v_1)
+        v_1_norm = v_1 / np.linalg.norm(v_1)
         v_2 = coordinates[ligand_index_2 - 1] - coordinates[metal_index - 1]
-        v_2 /= np.linalg.norm(v_2)
+        v_2_norm = v_2 / np.linalg.norm(v_2)
 
         # Calculate angle between vectors
-        angle_rad = np.arctan2(np.linalg.norm(np.cross(v_1, v_2)), np.dot(v_1, v_2))
+        angle_rad = np.arctan2(
+            np.linalg.norm(np.cross(v_1_norm, v_2_norm)), np.dot(v_1_norm, v_2_norm)
+        )
         angle = np.rad2deg(angle_rad)
 
         # Check if angle should be inverted
+        if ref_atoms is not None:
+            ref_vector = (
+                np.mean(coordinates[[i - 1 for i in ref_atoms]], axis=0)
+                - coordinates[metal_index - 1]
+            )
         inverted = False
         if ref_vector is not None:
-            ref_vector_norm = ref_vector / np.linalg.norm(ref_vector)
-            if np.dot(v_1 + v_2, ref_vector_norm) < 0:
+            ref_vector /= np.linalg.norm(ref_vector)
+            v_mean = (v_1 + v_2) / 2
+            v_mean /= np.linalg.norm(v_mean)
+            print(ref_vector, v_mean, np.dot(v_mean, ref_vector))
+            if np.dot(v_mean, ref_vector) < 0:
                 inverted = True
                 angle = 360 - angle
 
