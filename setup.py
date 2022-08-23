50c769151ba8136382e064db6da5937ccd73fb85
Kjell Jorner <36157530+kjelljorner@users.noreply.github.com>
2022-08-23 16:59:45 -0400

    Update changelog and version (#47)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ LONG_DESCRIPTION = f"""\
 
 setuptools.setup(
     name="morfeus-ml",
-    version="0.7.1",
+    version="0.7.2",
     author="Kjell Jorner",
     author_email="kjell.jorner@gmail.com",
     description=DESCRIPTION,
