2045893ce966bbd542a45e87518d2ab4862b71c1
Lauriane Jacot-Descombes <lauriane.jacot-descombes@chem.ethz.ch>
2025-11-11 11:12:24 +0100

    build: update to version 0.8.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ LONG_DESCRIPTION = f"""\
 
 setuptools.setup(
     name="morfeus-ml",
-    version="0.7.2",
+    version="0.8.0",
     author="Kjell Jorner",
     author_email="kjell.jorner@gmail.com",
     description=DESCRIPTION,
